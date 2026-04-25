cb336e8489a78c589ba8fdda945b71cacdc220fa
andrezottis <zottis.andre@outlook.com>
2026-04-25 14:05:16 -0300

    Game is giving a winner message in the wining conditions.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,36 +1,15 @@
-const initialBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialBoard;
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-  // const [gameBoard, setGameBoard] = useState(initialBoard);
-
-  // function handleSelectSquare(rowIndex, colIndex){
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-  //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedGameBoard;
-  //   });
-  //   onSelectSquare();
-  // }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled={playerSymbol !== null}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
