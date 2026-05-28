c8cd5e98861386dfa3d95b1f46c60c42b7368d35
andrezottis <zottis.andre@outlook.com>
2026-05-28 10:03:20 -0300

    Improved states for timerChallenge

--- src/components/TimerChallange.jsx
+++ src/components/TimerChallange.jsx
@@ -1,15 +1,32 @@
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStarted, setTimerStarted] = useState(false);
+
+  function handleStart() {
+    setTimerStarted(true);
+    setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
+
+  function handleStop() {
+    
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={handleStart}>
+          {timerStarted ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">
-        Time is running... / Timer inactive
+      <p className={timerStarted ? "active" : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
       </p>
     </section>
   );
