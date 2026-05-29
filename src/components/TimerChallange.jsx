f76b7112cdfed1159b935feb201d2d5dc7fb816a
André Zottis <zottis.andre@outlook.com>
2026-05-28 21:16:07 -0300

    using refs for values like useState.

--- src/components/TimerChallange.jsx
+++ src/components/TimerChallange.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
 
   function handleStart() {
     setTimerStarted(true);
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
   }
 
-  function handleStop() {}
+  function handleStop() {
+    clearTimeout(timer.current);
+    setTimerStarted(false);
+  }
 
   return (
     <section className="challenge">
@@ -21,7 +25,7 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
