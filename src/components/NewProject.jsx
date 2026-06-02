a761270c02b420d8ece3c154f29402910bf8775e
Andre Zottis <andre.zottis@capgemini.com>
2026-06-02 17:01:25 -0300

    Added new reusable components.

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,21 @@
+import Input from "./Input.jsx";
+
+export default function NewProject() {
+  return (
+    <div>
+      <menu>
+        <li>
+          <button>Cancel</button>
+        </li>
+        <li>
+          <button>Save</button>
+        </li>
+      </menu>
+      <div>
+        <Input label="Title" />
+        <Input label="Description" isTextArea />
+        <Input label="Due Date" type="date" />
+      </div>
+    </div>
+  );
+}
