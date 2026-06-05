a01735c7b31db62afc29836b98db98f164a604b2
andrezottis <zottis.andre@outlook.com>
2026-06-05 11:05:47 -0300

    collected values entered in inputs.

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,20 +1,46 @@
 import Input from "./Input.jsx";
+import { useRef } from "react";
 
-export default function NewProject() {
+export default function NewProject({ onAdd }) {
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+  const dueDataRef = useRef();
+
+  function handleSave() {
+    const enteredTitle = titleRef.current.value;
+    const enteredDescription = descriptionRef.current.value;
+    const enteredDueDate = dueDataRef.current.value;
+
+    //Add validation here
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="text-stone-800 hover:text-stone-900">Cancel</button>
+          <button className="text-stone-800 hover:text-stone-900">
+            Cancel
+          </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
+          <button
+            onClick={handleSave}
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+          >
+            Save
+          </button>
         </li>
       </menu>
       <div>
-        <Input label="Title" />
-        <Input label="Description" isTextArea />
-        <Input label="Due Date" type="date" />
+        <Input ref={titleRef} label="Title" />
+        <Input ref={descriptionRef} label="Description" isTextArea />
+        <Input ref={dueDataRef} label="Due Date" type="date" />
       </div>
     </div>
   );
