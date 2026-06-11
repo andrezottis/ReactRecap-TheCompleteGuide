0c7257f7ec050409485a22a9d06361396bee6eaf
andrezottis <zottis.andre@outlook.com>
2026-06-10 21:57:05 -0300

    Styled new dialog for modal

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -32,9 +32,9 @@ export default function NewProject({ onAdd }) {
   return (
     <>
       <Modal ref={modal} buttonCaption="Close">
-        <h2>Invalid Input</h2>
-        <p>Forgot to enter a value.</p>
-        <p>Populate all fields.</p>
+        <h2 className="text-xl font-bold text-stone-600 my-4">Invalid Input</h2>
+        <p className="text-stone-500 mb-4">Forgot to enter a value.</p>
+        <p className="text-stone-500 mb-4">Populate all fields.</p>
       </Modal>
       <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
