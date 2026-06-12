1c2edaca11e4fc4270ecab75b021ae7cb8ddc876
andrezottis <zottis.andre@outlook.com>
2026-06-11 22:55:38 -0300

    Fixing bugs and finishing the tasks component.

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,14 @@ function App() {
     });
   }
 
-  function handleDeleteTask() {}
+  function handleDeleteTask(id) {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
 
   function handleCancelAddProject() {
     setProjectsState((prevState) => {
@@ -109,6 +116,7 @@ function App() {
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
         onSelectProject={handleSelectProject}
+        selectedProjectId={projectsState.selectedProjectId}
       />
       {content}
     </main>
