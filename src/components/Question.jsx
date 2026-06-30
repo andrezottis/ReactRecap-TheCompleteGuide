dda30eb15179695b3716eceeb9464310370fb9d4
andrezottis <zottis.andre@outlook.com>
2026-06-29 23:06:58 -0300

    Improved code to have multiple components.

---
+++ src/components/Question.jsx
@@ -0,0 +1,20 @@
+import QuestionTimer from "./QuestionTimer";
+import Answers from "./Answers.jsx";
+
+export default function Question({ questionText, answers, onSelectAnswer, selectedAnswer, answerState, onSkip }) {
+  return (
+    <div id="question">
+      <QuestionTimer
+        timeout={10000}
+        onTimeout={onSkip}
+      />
+      <h2>{questionText}</h2>
+      <Answers
+        answers={answers}
+        selectedAnswer={selectedAnswer}
+        answerState={answerState}
+        onSelect={onSelectAnswer}
+      />
+    </div>
+  );
+}
