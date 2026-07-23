53b7f978a16d591772fd57e9a8243e22fb011692
andrezottis <zottis.andre@outlook.com>
2026-07-23 09:48:08 -0300

    making available the fetch with useEffect

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,16 +1,18 @@
 import Places from "./Places.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  fetch("http://localhost:3000/places")
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setAvailablePlaces(data.places);
-    });
+  useEffect(() => {
+    fetch("http://localhost:3000/places")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setAvailablePlaces(data.places);
+      });
+  }, []);
 
   return (
     <Places
