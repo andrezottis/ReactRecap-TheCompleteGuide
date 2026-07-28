b24beba5e8ad9138e3a6948316617368e49faf4e
andrezottis <zottis.andre@outlook.com>
2026-07-27 22:16:00 -0300

    sorting based on geolocation

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import ErrorPage from "./ErrorPage.jsx";
 import Places from "./Places.jsx";
 import { useState, useEffect } from "react";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -17,11 +18,19 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw new Error("Fail to perform the request.");
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude,
+          );
+          setAvailablePlaces(sortedPlaces);
+        });
       } catch (error) {
         setError({
           message: error.message || "Failed while fetching data. Try again.",
         });
+        setIsFetching(false);
       }
     }
 
