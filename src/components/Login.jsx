70432395e79f48b0e2a67562a0a673aeed588de7
Andre Zottis <andre.zottis@capgemini.com>
2026-08-31 11:25:58 -0300

    handling inputs with useState

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
+
   function handleSubmit(event) {
     event.preventDefault();
-    const formData = new FormData(event.target);
-    const email = formData.get('email');
-    const password = formData.get('password');
-    console.log({ email, password });
+    console.log({ enteredValues });
   }
-  
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
+      [identifier]: value,
+    }));
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -14,17 +25,42 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) =>
+              handleInputChange("password", event.target.value)
+            }
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
       <p className="form-actions">
-        <button className="button button-flat">Reset</button>
+        <button
+          type="button"
+          className="button button-flat"
+          onClick={() =>
+            setEnteredValues({
+              email: "",
+              password: "",
+            })
+          }
+        >
+          Reset
+        </button>
         <button className="button">Login</button>
       </p>
     </form>
