a203c82b4228129b40eb2fdc1006dedd6b1d6eb7
Andre Zottis <andre.zottis@capgemini.com>
2026-09-01 11:28:22 -0300

    browser validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/Header.jsx";
 import Login from "./components/Login.jsx";
-import Singup from "./components/Singup.jsx";
+import Signup from "./components/Signup.jsx";
 //import Login from "./components/StateLogin.jsx";
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -16,7 +16,7 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email" required minLength={6} />
       </div>
 
       <div className="control-row">
@@ -51,7 +51,7 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select id="role" name="role" required>
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -90,7 +90,7 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          <input type="checkbox" id="terms-and-conditions" name="terms"  required/>I
           agree to the terms and conditions
         </label>
       </div>
