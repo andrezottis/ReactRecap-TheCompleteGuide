6a44dadcc40f1e983f0f3b9579c73bbb022f352f
andrezottis <zottis.andre@outlook.com>
2026-08-31 22:05:22 -0300

    validation every key press

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/Header.jsx";
-import Login from "./components/Login.jsx";
+//import Login from "./components/Login.jsx";
 import Singup from "./components/Singup.jsx";
-
+import Login from "./components/StateLogin.jsx";
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Singup />
+        <Login />
       </main>
     </>
   );
