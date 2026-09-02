b09b835b0d6eafcbd4d01ae97c3e3d36f4c9903a
andrezottis <zottis.andre@outlook.com>
2026-09-01 21:25:24 -0300

    outsourcing validation logic

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Input from "./Input.jsx";
+import { isEmail, isNotEmpty, hasMinLength } from "../util/validation.js";
 
 export default function Login() {
   const [enteredValues, setEnteredValues] = useState({
@@ -12,9 +13,12 @@ export default function Login() {
     password: false,
   });
 
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+  const emailIsInvalid =
+    didEdit.email &&
+    !isEmail(enteredValues.email) &&
+    !isNotEmpty(enteredValues.email);
   const passwordIsInvalid =
-    didEdit.password && enteredValues.password.trim().length < 6;
+    didEdit.password && !hasMinLength(enteredValues.password, 6);
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -63,7 +67,7 @@ export default function Login() {
           onBlur={() => handleInputBlur("email")}
           onChange={(event) => handleInputChange("email", event.target.value)}
           value={enteredValues.email}
-          error={emailIsInvalid && 'Please enter a valid e-mail.'}
+          error={emailIsInvalid && "Please enter a valid e-mail."}
         />
 
         <Input
@@ -76,7 +80,7 @@ export default function Login() {
             handleInputChange("password", event.target.value)
           }
           value={enteredValues.password}
-          error={passwordIsInvalid && 'Please add a valid password.'}
+          error={passwordIsInvalid && "Please add a valid password."}
         />
       </div>
 
