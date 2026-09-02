09f27d09a9a8d52002e5b92a652d72a05096579e
Andre Zottis <andre.zottis@capgemini.com>
2026-09-02 10:40:53 -0300

    form state dependent

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,5 @@
+import { useActionState } from "react";
+
 import {
   isEmail,
   hasMinLength,
@@ -6,7 +8,7 @@ import {
 } from "../util/validation.js";
 
 export default function Signup() {
-  function signupAction(formData) {
+  function signupAction(prevFormState, formData) {
     const email = formData.get("email");
     const password = formData.get("password");
     const confirmPassword = formData.get("confirm-password");
@@ -50,12 +52,18 @@ export default function Signup() {
     }
     if (errors.length > 0) {
       console.log("Validation errors:", errors);
-      return;
+      return { errors: errors };
     }
+
+    return { errors: null };
   }
 
+  const [formState, formAction] = useActionState(signupAction, {
+    errors: null,
+  });
+
   return (
-    <form action={signupAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -139,6 +147,14 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+      {formState.errors && (
+        <ul className="error">
+          {formState.errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
