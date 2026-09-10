53ab71925bff3cfe4e5574aff22b1d646cc4ac3f
Andre Zottis <andre.zottis@capgemini.com>
2026-09-10 11:11:32 -0300

    Using userInput to update the default value

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -52,7 +52,19 @@ export default function Signup() {
     }
     if (errors.length > 0) {
       console.log("Validation errors:", errors);
-      return { errors: errors };
+      return {
+        errors: errors,
+        enteredValues: {
+          email,
+          password,
+          confirmPassword,
+          firstName,
+          lastName,
+          role,
+          acquisitionChannel,
+          agreedToTerms,
+        },
+      };
     }
 
     return { errors: null };
@@ -69,13 +81,23 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input
+          id="email"
+          type="email"
+          name="email"
+          defaultValue={formState.enteredValues?.email || ""}
+        />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            defaultValue={formState.enteredValues?.password || ""}
+          />
         </div>
 
         <div className="control">
@@ -84,6 +106,7 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            defaultValue={formState.enteredValues?.confirmPassword || ""}
           />
         </div>
       </div>
@@ -93,18 +116,32 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input
+            type="text"
+            id="first-name"
+            name="first-name"
+            defaultValue={formState.enteredValues?.firstName || ""}
+          />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input
+            type="text"
+            id="last-name"
+            name="last-name"
+            defaultValue={formState.enteredValues?.lastName || ""}
+          />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select
+          id="role"
+          name="role"
+          defaultValue={formState.enteredValues?.role || ""}
+        >
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -121,6 +158,10 @@ export default function Signup() {
             id="google"
             name="acquisition"
             value="google"
+            defaultValue={
+              formState.enteredValues?.acquisitionChannel?.includes("google") ||
+              false
+            }
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -131,20 +172,38 @@ export default function Signup() {
             id="friend"
             name="acquisition"
             value="friend"
+            defaultValue={
+              formState.enteredValues?.acquisitionChannel?.includes("friend") ||
+              false
+            }
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input
+            type="checkbox"
+            id="other"
+            name="acquisition"
+            value="other"
+            defaultValue={
+              formState.enteredValues?.acquisitionChannel?.includes("other") ||
+              false
+            }
+          />
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
-          agree to the terms and conditions
+          <input
+            type="checkbox"
+            id="terms-and-conditions"
+            name="terms"
+            defaultValue={formState.enteredValues?.agreedToTerms || false}
+          />
+          I agree to the terms and conditions
         </label>
       </div>
 
