3157d2b73fb22b4598bcefe5a6e3c14c6c818713
andrezottis <zottis.andre@outlook.com>
2026-09-17 10:16:07 -0300

    creating useHooks to httprequests

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,27 +1,19 @@
-import { useState, useEffect } from "react";
+import useHttp from "../hooks/useHttp";
 import MealItem from "./MealItem";
 
+
+const requestConfig = {};
+
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  useEffect(() => {
-    async function fetchMeals() {
-      try {
-        const response = await fetch("http://localhost:3000/meals"); //GET is the default method.
-
-        if (!response.ok) {
-          //... treat the error.
-        }
-
-        const meals = await response.json();
-        setLoadedMeals(meals);
-      } catch {
-        //do something
-      }
-    }
-
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p> Fetching meals...</p>;
+  }
 
   return (
     <ul id="meals">
---
+++ src/hooks/useHttp.js
@@ -0,0 +1,45 @@
+import { useCallback, useEffect, useState } from "react";
+
+async function sendHttpRequest(url, configuration) {
+  const response = await fetch(url, configuration);
+
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error(resData.message || "Something wrong happened.");
+  }
+
+  return resData;
+}
+
+export default function useHttp(url, configuration, initialData) {
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState(initialData);
+
+  const sendRequest = useCallback(
+    async function sendRequest() {
+      setIsLoading(true);
+      try {
+        const resData = await sendHttpRequest(url, configuration);
+        setData(resData);
+      } catch (error) {
+        setError(error.message || "Something went wrong.");
+      }
+      setIsLoading(false);
+    },
+    [url, configuration],
+  );
+
+  useEffect(() => {
+    if (
+      (configuration &&
+        (configuration.method === "GET" || !configuration.method)) ||
+      !configuration
+    ) {
+      sendRequest();
+    }
+  }, [sendRequest, configuration]);
+
+  return { data, isLoading, error, sendRequest };
+}
