7cd50b8db38d9c8e553823ef9c2c715e731ad103
Andre Zottis <andre.zottis@capgemini.com>
2026-09-23 11:48:23 -0300

    added mode slice to auth components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import Counter from './components/Counter';
-
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
 
 function App() {
+const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
+      {isAuthenticated && <Counter />}
+    </Fragment>
   );
 }
 
