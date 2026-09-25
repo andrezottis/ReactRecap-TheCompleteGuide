597cc8ef83fc93bd4706649984b0e5e2a91d9fe4
andrezottis <zottis.andre@outlook.com>
2026-09-24 21:40:03 -0300

    added base for cart-slice

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -5,6 +5,7 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
+
   },
   reducers: {
     addItemToCart(state, action) {
@@ -19,10 +20,23 @@ const cartSlice = createSlice({
           name: newItem.title,
         });
       } else {
-        existingItem.quantity ++;
-        existingItem.totalPrice = totalPrice + newItem.price;
+        existingItem.quantity++;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
-    removeItemFromCart() {},
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
   },
 });
+
+export const cartActions = cartSlice.actions;
+
+export default cartSlice;
